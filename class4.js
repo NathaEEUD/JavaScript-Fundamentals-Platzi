d2850166a3a9d65f929766b077ab2f5b1b8c2252
Nathaly Riaño Bejarano <natharb09@gmail.com>
2019-01-16 18:39:35 -0500

    feat: Functions in JS

---
+++ class4.js
@@ -0,0 +1,14 @@
+var name = 'Nathaly';
+var age = 26;
+
+function printAge() {
+    console.log(`${name} tiene ${age} años`);
+}
+
+printAge();
+
+function printAgeArgs(n, e) {
+    console.log(`${n} tiene ${e} años`);
+}
+
+printAgeArgs(name, age);
