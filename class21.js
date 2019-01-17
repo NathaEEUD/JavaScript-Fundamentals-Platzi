2aed6d4b4198f01fb7fb9dd32c6e1113657c2bfe
Nathaly Riaño Bejarano <natharb09@gmail.com>
2019-01-17 18:49:38 -0500

    feat: How classes work in JS

---
+++ class21.js
@@ -0,0 +1,23 @@
+function Persona(nombre, apellido, altura) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+}
+
+const INDICE_DE_ALTURA = 1.8
+var esAlto = altura => altura > INDICE_DE_ALTURA
+
+Persona.prototype.saludar = function () {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+}
+
+Persona.prototype.clasificarAltura = function () {
+    if (esAlto(this.altura)) {
+        console.log(`La persona ${this.nombre} ${this.apellido} es alta`)
+    } else {
+        console.log(`La persona ${this.nombre} ${this.apellido} es baja`)
+    }
+}
+
+var natha = new Persona('Nathaly', 'Riaño', 1.58)
+natha.saludar()
